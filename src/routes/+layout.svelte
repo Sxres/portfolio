826375b1ca8e0a6dde6a2b0b9f7740f01f87d8b5
Passdaboof <sorescudragos847@gmail.com>
2026-03-05 01:55:30 -0500

    getting rid of duplicate meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,10 +6,6 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<meta property="og:title" content="sxres.dev" />
-  	<meta property="og:description" content="portfolio site with procedurally generated bonsai trees" />
-  	<meta property="og:image" content="https://sxres.dev/port.jpg" />
-  	<meta property="og:url" content="https://sxres.dev" />
 </svelte:head>
 
 {@render children()}
